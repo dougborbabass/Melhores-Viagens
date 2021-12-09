c0d2db9aec3bbde506d69d0d6fc3b7355415d1fb
Douglas Borba <douglas.borba@dbccompany.com.br>
2021-12-09 16:25:00 -0300

    bind dos dados do resumo do pacote

--- app/src/main/java/br/com/douglas/melhoresviagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/br/com/douglas/melhoresviagens/ui/activity/ListaPacotesActivity.java
@@ -2,6 +2,7 @@ package br.com.douglas.melhoresviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -22,8 +23,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_pacotes);
 
         setTitle(TITULO_APP_BAR);
-
         configuraLista();
+
+        Intent intent = new Intent(this, ResumoPacoteActivity.class);
+        startActivity(intent);
     }
 
     private void configuraLista() {
--- app/src/main/java/br/com/douglas/melhoresviagens/ui/activity/ResumoPacoteActivity.java
+++ app/src/main/java/br/com/douglas/melhoresviagens/ui/activity/ResumoPacoteActivity.java
@@ -2,9 +2,22 @@ package br.com.douglas.melhoresviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import br.com.douglas.melhoresviagens.R;
+import br.com.douglas.melhoresviagens.model.Pacote;
+import br.com.douglas.melhoresviagens.util.DiasUtil;
+import br.com.douglas.melhoresviagens.util.MoedaUtil;
+import br.com.douglas.melhoresviagens.util.ResourcesUtil;
 
 public class ResumoPacoteActivity extends AppCompatActivity {
 
@@ -12,5 +25,35 @@ public class ResumoPacoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resumo_pacote);
+
+        setTitle("Resumo do pacote");
+
+        Pacote pacoteSP = new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal("243.99"));
+
+        TextView tv_local = findViewById(R.id.resumo_pacote_local);
+        tv_local.setText(pacoteSP.getLocal());
+
+        ImageView im_local = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourcesUtil.devolveUmDrawable(this, pacoteSP.getImagem());
+        im_local.setImageDrawable(drawableDoPacote);
+
+        TextView tv_dias = findViewById(R.id.resumo_pacote_dias);
+        String diasEmTexto = DiasUtil.formataEmdias(pacoteSP.getDias());
+        tv_dias.setText(diasEmTexto);
+
+        TextView tv_preco = findViewById(R.id.resumo_pacote_preco);
+        String precoFormatadoBR = MoedaUtil.formataMoedaParaBR(pacoteSP.getPreco());
+        tv_preco.setText(precoFormatadoBR);
+
+        TextView tv_data = findViewById(R.id.resumo_pacote_data);
+        Calendar dataIda = Calendar.getInstance();
+        Calendar dataVolta = Calendar.getInstance();
+        dataVolta.add(Calendar.DATE, pacoteSP.getDias());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM");
+        String dataIdaFormatada = sdf.format(dataIda.getTime());
+        String dataVoltaFormatada = sdf.format(dataVolta.getTime());
+        String dataFormatadaDaViagem = dataIdaFormatada + " - " + dataVoltaFormatada + " de " + dataVolta.get(Calendar.YEAR);
+        tv_data.setText(dataFormatadaDaViagem);
+
     }
 }
